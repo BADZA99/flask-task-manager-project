9106a71aefa255d865008d970e2cedbc07fd8a3a
papabn <pndiaye999@gmail.com>
2023-06-13 01:49:32 +0000

    add sign in page

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def index():
 def base():
     return render_template("base.html")
 
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
 if __name__== "__main__":
     app.run()
