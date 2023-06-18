e822bb3de76f8c9d28dcbae333a4175960c3810f
papabn <pndiaye999@gmail.com>
2023-06-18 02:49:19 +0000

    add login log out add task  & readtask functions

---
+++ models.py
@@ -0,0 +1,84 @@
+import psycopg2
+
+def get_connexion():
+    con=psycopg2.connect(
+        database="flaskdb",
+        password="passer",
+        host="localhost",
+        user="postgres"
+    )
+    cur=con.cursor()
+    return con,cur
+
+
+# def create_table():
+#     con,cur =get_connexion()
+#     requete ="CREATE TABLE IF NOT EXISTS utilisateur(idU SERIAL PRIMARY KEY,email VARCHAR(255),login VARCHAR(25),mdp VARCHAR(20))"
+#     cur.execute(requete)
+#     con.commit()
+#     con.close()
+
+# create_table()
+
+# creer la table tache avec iduser comme cle etrangere provenant de utilisateur (idU) ainsi que titre,
+
+
+## creer un Etudiant
+def addUser(email,login1,mdp):
+    con,cur =get_connexion()
+    requete ="INSERT INTO utilisateur(email,login,mdp) VALUES (%s,%s,%s)"
+    cur.execute(requete,(email,login1,mdp))
+    con.commit()
+    print("utilisateur ajoute avec success !!!")
+    con.close()    
+
+
+
+def VerifUser(login1,mdp):
+    con,cur =get_connexion()
+    requete ="SELECT * FROM utilisateur where mdp=%s and login=%s"
+    cur.execute(requete,(mdp,login1))
+    utilisateur = cur.fetchone()
+    con.close() 
+    return utilisateur
+    
+## pour les taches
+def addTask(idUser,titre,description,date):
+    con,cur =get_connexion()
+    requete ="INSERT INTO tache(iduser,titre,datedeche,description) VALUES (%s,%s,%s,%s)"
+    cur.execute(requete,(idUser,titre,date,description))
+    con.commit()
+    print("tache ajoute avec success !!!")
+    con.close()    
+
+def readTasks(idUser):
+    con,cur =get_connexion()
+    requete ="SELECT * FROM tache,utilisateur where idu=%s and iduser=idu ORDER BY datedeche ASC"
+    cur.execute(requete,(idUser,))
+    tache = cur.fetchall()
+    con.close() 
+    return tache
+
+def readTask(id,idUser):
+    con,cur =get_connexion()
+    requete ="SELECT * FROM tache,utilisateur where idT=%s and idUser=%s and idUser=idU"
+    cur.execute(requete,(id,idUser,))
+    tache = cur.fetchone()
+    con.close() 
+    return tache
+    
+def modifyTask(id,titre,description,date):
+    con,cur =get_connexion()
+    requete ="UPDATE tache set titre=%s,description=%s,datedeche=%s where idT=%s"
+    cur.execute(requete,(titre,description,date,id))
+    con.commit()
+    print("tache modifier avec success !!!")
+    con.close()   
+
+def deleteTask(id,idU):
+    con,cur =get_connexion()
+    requete ="DELETE FROM tache where idT=%s and idUser=%s "
+    cur.execute(requete,(id,idU))
+    con.commit()
+    print("SUPPRESSION fait avec success !!!")
+    con.close()   
--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
+from models import *
 app= Flask(__name__)
 error=0
 data = None
@@ -7,16 +8,67 @@ def home():
     global data
     return render_template("base.html")
 
-@app.route('/signin')
+@app.route('/signin',methods=['GET','POST'])
 def signin():
     global data
+    if request.method=='POST':
+        addUser(email=request.form["email"],login1=request.form["username"],mdp=request.form["password"])
+        # Handle POST Request here
+        return redirect(url_for('login'))
     return render_template("signin.html")
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    global data
+    if request.method=='POST':
+        info=VerifUser(login1=request.form["username"],mdp=request.form["password"])
+        # Handle POST Request here
+        if info:
+            data=info[0]
+            # userTask=readTasks(userId)
+            # print(data)
+            return render_template('taches.html',data=data)
+            # return redirect(url_for('taches'))
+            
+        else:
+            error=1
+            return render_template('login.html',error=error)
+      
+          # return "<h1>MOT DE PASSE OU USERNAME INCORRECT</h1> <a href='/login'><h1>Retry</h1></a>"
+    else:
+        return render_template("login.html")
 
 
-@app.route('/taches')
-def taches():
-    return render_template("taches.html")
+@app.route('/taches/<int:idUser>',methods=['GET','POST'])
+def taches(idUser):
+    global data
+    data2=readTasks(idUser)
+    return render_template('affiche.html',data2=data2,data=data)
+
+
+@app.route('/addtask/<int:idUser>',methods=['GET','POST'])
+def addtask(idUser):
+    global data
+    if request.method=='POST':
+        print(idUser)
+        addTask(idUser,titre=request.form['title'],description=request.form['description'],date=request.form['datexp'])
+        data2=readTasks(idUser)
+        print(data2[0])
+        # Handle POST Request here
+        # if data== True:
+        return render_template('taches.html',data2=data2,data=data)
+
+    return render_template('addtask.html',data=data)
+    # return render_template('addtask.html')
+
+@app.route('/edit')
+def edittask():
+    global data
+    return render_template('edit.html')
+
+
+
+
+
+
+
